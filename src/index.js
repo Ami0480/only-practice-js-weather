5cffe58c48adfe09d078112dfc5bf4074859624e
Ami0480 <amifuku80@gmail.com>
2025-08-12 16:41:48 +0800

    Added current-temperature api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 function showElement(response) {
   console.log(response.data);
   let searchCity = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#current-temperature");
 
   searchCity.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 function displayCity(city) {
   let apiKey = "d1193959d2d841ec7555416d715716a6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showElement);
 }
 
