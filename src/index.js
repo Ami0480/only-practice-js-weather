e79cc388bfffbe9fe99feb5e024759ffd3ded1a1
Ami0480 <amifuku80@gmail.com>
2025-08-12 16:19:58 +0800

    Added search city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("Hello");
+function searchForCity(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-form-input");
+  let searchCity = document.querySelector("#city");
+
+  searchCity.innerHTML = searchInput.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchForCity);
