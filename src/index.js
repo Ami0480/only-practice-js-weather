f6a5c5b86867eae17f48e52e88c8deda102aa47c
Ami0480 <amifuku80@gmail.com>
2025-08-12 16:32:28 +0800

    Added api and search city

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+function showElement(response) {
+  console.log(response.data);
+  let searchCity = document.querySelector("#city");
+
+  searchCity.innerHTML = response.data.name;
+}
+
+function displayCity(city) {
+  let apiKey = "d1193959d2d841ec7555416d715716a6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showElement);
+}
+
 function searchForCity(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let searchCity = document.querySelector("#city");
 
-  searchCity.innerHTML = searchInput.value;
+  displayCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
